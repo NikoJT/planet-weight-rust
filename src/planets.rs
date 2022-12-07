b0be298870f4488b01cb0e782bb198b6f197ef5d
NikoJT <niko.tarmaa@gmail.com>
2022-12-07 14:04:34 +0200

    cli emojis

--- src/planets.rs
+++ src/planets.rs
@@ -7,7 +7,7 @@ struct Planet {
 }
 
 fn calculate_weight(weight: f32, planet: &Planet) -> f32 {
-    let weight = (weight / 9.81) * planet.gravity;
+    let weight:f32 = (weight / 9.81) * planet.gravity;
     return weight;
 }
 
@@ -21,35 +21,35 @@ fn print_welcome_text() {
 
 pub(crate) fn planets() {
     let entities:HashMap<String, f32> = HashMap::from([
-        ("Mercury".to_string(), 3.7),
-        ("Venus".to_string(), 8.87),
-        ("Earth".to_string(), 9.81),
+        ("Mercury 🪐".to_string(), 3.7),
+        ("Venus 🪐".to_string(), 8.87),
+        ("Earth 🌍".to_string(), 9.81),
         ("Mars".to_string(), 3.721),
         ("Jupiter".to_string(), 24.79),
-        ("Saturn".to_string(), 10.44),
-        ("Uranus".to_string(), 8.87),
-        ("Neptune".to_string(), 11.15),
-        ("Pluto".to_string(), 0.62),
-        ("Moon".to_string(), 1.62),
-        ("Europa".to_string(), 1.315),
-        ("Ganymede".to_string(), 1.428),
-        ("Io".to_string(), 1.796),
-        ("Callisto".to_string(), 1.236),
-        ("Titan".to_string(), 1.352),
-        ("Triton".to_string(), 0.779),
-        ("Charon".to_string(), 0.288),
-        ("Phobos".to_string(), 0.0057),
-        ("Deimos".to_string(), 0.003),
-        ("Phoebe".to_string(), 0.049),
-        ("Lapetus".to_string(), 0.223),
-        ("Hyperion".to_string(), 0.02),
-        ("Rhea".to_string(), 0.264),
-        ("Dione".to_string(), 0.232),
-        ("Tethys".to_string(), 0.145),
-        ("Enceladus".to_string(), 0.113),
-        ("Mimas".to_string(), 0.064),
-        ("Nereid".to_string(), 0.071),
-        ("Proteus".to_string(), 0.07),
+        ("Saturn 🪐".to_string(), 10.44),
+        ("Uranus 🪐".to_string(), 8.87),
+        ("Neptune 🪐".to_string(), 11.15),
+        ("Pluto 🪐".to_string(), 0.62),
+        ("Moon 🌔".to_string(), 1.62),
+        ("Europa 🌔".to_string(), 1.315),
+        ("Ganymede 🌔".to_string(), 1.428),
+        ("Io 🌔".to_string(), 1.796),
+        ("Callisto 🌔".to_string(), 1.236),
+        ("Titan 🌔".to_string(), 1.352),
+        ("Triton 🌔".to_string(), 0.779),
+        ("Charon 🌔".to_string(), 0.288),
+        ("Phobos ☄".to_string(), 0.0057),
+        ("Deimos  ☄".to_string(), 0.003),
+        ("Phoebe ☄".to_string(), 0.049),
+        ("Lapetus 🌔".to_string(), 0.223),
+        ("Hyperion ☄".to_string(), 0.02),
+        ("Rhea 🌔".to_string(), 0.264),
+        ("Dione 🌔".to_string(), 0.232),
+        ("Tethys 🌔".to_string(), 0.145),
+        ("Enceladus 🌔".to_string(), 0.113),
+        ("Mimas ☄".to_string(), 0.064),
+        ("Nereid ☄".to_string(), 0.071),
+        ("Proteus ☄".to_string(), 0.07),
     ]);
 
     print_welcome_text();
